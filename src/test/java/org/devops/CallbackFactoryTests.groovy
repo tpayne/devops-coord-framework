ae4ab02b0498fb509e6b3e09bbdc0e71d01ef2c2
Tim Payne <tim.tpayne@gmail.com>
2019-03-11 20:34:10 +0000

    Factory unit-tests

---
+++ src/test/java/org/devops/CallbackFactoryTests.groovy
@@ -0,0 +1,26 @@
+package org.devops;
+
+import groovy.util.GroovyTestCase;
+import junit.framework.*;
+import junit.textui.TestRunner;
+
+/**
+ * Unit CallbackFactory tests for CallbackFactory class.
+ */
+public class CallbackFactoryTests extends GroovyTestCase {
+
+      void testGetCallback() {
+         CallbackFactory cb = new CallbackFactory()
+         Map bld = cb.getCallbacks(new Build(null,null),null)
+         Map dpl = cb.getCallbacks(new Deploy(null,null),null)
+         Map tst = cb.getCallbacks(new Test(null,null),null)
+         Map itg = cb.getCallbacks(new Integration(null,null),null)
+         Map rc = cb.getCallbacks(new ReleaseCandidate(null,null),null)
+
+         assertEquals(bld,CallbackConstants.BuildCallbackMap)
+         assertEquals(dpl,CallbackConstants.DeployCallbackMap)
+         assertEquals(tst,CallbackConstants.TestCallbackMap)
+         assertEquals(itg,CallbackConstants.IntegrationCallbackMap)
+         assertEquals(rc,CallbackConstants.ReleaseCandCallbackMap)
+      }
+}
