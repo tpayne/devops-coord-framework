7ab1a38b0e99016e6470a4dac1b8b93af3ecbcbb
Tim Payne <tim.tpayne@gmail.com>
2019-03-26 23:06:53 +0000

    Docker work

--- src/main/vars/defaultUnixDevTemplate.groovy
+++ src/main/vars/defaultUnixDevTemplate.groovy
@@ -2,7 +2,7 @@ def call(Closure body) {
     podTemplate(
         label: 'default',
         containers: [
-            containerTemplate(name: 'linux-dev', image: "rhub/ubuntu-gcc-release", alwaysPullImage: true, resourceRequestMemory: '4Gi', resourceLimitMemory: '4Gi', resourceRequestCpu: '500m'),
+            containerTemplate(name: 'linux-dev', image: "linux-dev", alwaysPullImage: true, resourceRequestMemory: '4Gi', resourceLimitMemory: '4Gi', resourceRequestCpu: '500m'),
         ])
     {
         body()
--- src/main/vars/defaultUnixTemplate.groovy
+++ src/main/vars/defaultUnixTemplate.groovy
@@ -2,7 +2,7 @@ def call(Closure body) {
     podTemplate(
         label: 'default',
         containers: [
-            containerTemplate(name: 'linux-base', image: "ubuntu", alwaysPullImage: true, resourceRequestMemory: '4Gi', resourceLimitMemory: '4Gi', resourceRequestCpu: '500m'),
+            containerTemplate(name: 'linux-std', image: "linux-std", alwaysPullImage: true, resourceRequestMemory: '4Gi', resourceLimitMemory: '4Gi', resourceRequestCpu: '500m'),
         ])
     {
         body()
