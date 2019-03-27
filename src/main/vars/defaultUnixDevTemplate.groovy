02307e7d797b518ea2ca9d4c9b5c2ae0c6dd2811
Tim Payne <tim.tpayne@gmail.com>
2019-03-27 18:16:28 +0000

    Container changes

--- src/main/vars/defaultUnixDevTemplate.groovy
+++ src/main/vars/defaultUnixDevTemplate.groovy
@@ -2,7 +2,7 @@ def call(Closure body) {
     podTemplate(
         label: 'default',
         containers: [
-            containerTemplate(name: 'linux-dev', image: "linux-dev", alwaysPullImage: true, resourceRequestMemory: '4Gi', resourceLimitMemory: '4Gi', resourceRequestCpu: '500m'),
+            containerTemplate(name: 'linux-dev', image: "localhost:5000/server/linux-dev:0.1", alwaysPullImage: true, resourceRequestMemory: '4Gi', resourceLimitMemory: '4Gi', resourceRequestCpu: '500m'),
         ])
     {
         body()
--- src/main/vars/defaultUnixTemplate.groovy
+++ src/main/vars/defaultUnixTemplate.groovy
@@ -2,7 +2,7 @@ def call(Closure body) {
     podTemplate(
         label: 'default',
         containers: [
-            containerTemplate(name: 'linux-std', image: "linux-std", alwaysPullImage: true, resourceRequestMemory: '4Gi', resourceLimitMemory: '4Gi', resourceRequestCpu: '500m'),
+            containerTemplate(name: 'linux-std', image: "localhost:5000/server/linux-std:0.1", alwaysPullImage: true, resourceRequestMemory: '4Gi', resourceLimitMemory: '4Gi', resourceRequestCpu: '500m'),
         ])
     {
         body()
