079652e83321ab26f8cf5808152f9054d00ceac8
Tim Payne <tpayne@users.noreply.github.com>
2021-08-24 11:56:28 +0100

    Create runJenkinsDocker.sh

---
+++ runJenkinsDocker.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Build docker image...
+docker build . -f JenkinsDockerfile.docker -t jenkinsdocker:latest
+
+# Create & install network service...
+docker network create jenkins
+docker run --name jenkins-docker --rm --detach \
+  --privileged --network jenkins --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 docker:dind --storage-driver overlay2
+
+# Run docker..
+docker run --name jenkinsdocker -it  \
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+  --publish 85:8080 --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  jenkinsdocker:latest
